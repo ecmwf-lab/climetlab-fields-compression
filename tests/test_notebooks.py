e9f6b9ebed0da680a1a1d9a8c28e1788385a8638
dmey <dmey@users.noreply.github.com>
2022-06-17 11:40:02 +0200

    cleanup

--- tests/test_notebooks.py
+++ tests/test_notebooks.py
@@ -1,4 +1,3 @@
-#!/usr/bin/env python3
 # (C) Copyright 2022 European Centre for Medium-Range Weather Forecasts.
 # This software is licensed under the terms of the Apache Licence Version 2.0
 # which can be obtained at http://www.apache.org/licenses/LICENSE-2.0.
